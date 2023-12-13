80ec03f8ac20f6d45a7df292cfe92aed80e18ae1
Lucca Sabbatini Reid Rodrigues <sabbatini.lucca@gmail.com>
2023-12-13 16:32:49 -0300

    feat: Add create mesh, add rectangle and add quadrilateral

--- hetool_modeler/curve_collector.py
+++ hetool_modeler/curve_collector.py
@@ -1,6 +1,8 @@
 from PyQt5.QtWidgets import *
 from OpenGL.GL import *
 from PyQt5.QtGui import *
+from hetool.include.hetool import Hetool
+from hetool.geometry.point import Point
 
 
 class AppCurveCollector:
@@ -44,6 +46,22 @@ class AppCurveCollector:
                 elif len(self.m_ctrlPts) == 2:
                     self.m_ctrlPts.append([_x, _y])
                     isComplete = True
+            elif self.m_curveType == "Rectangular":
+                if len(self.m_ctrlPts) == 0:
+                    self.m_ctrlPts.append([_x, _y])
+                elif len(self.m_ctrlPts) == 1:
+                    self.m_ctrlPts.append([_x, _y])
+                    isComplete = True
+            elif self.m_curveType == "Quadrilateral":
+                if len(self.m_ctrlPts) == 0:
+                    self.m_ctrlPts.append([_x, _y])
+                elif len(self.m_ctrlPts) == 1:
+                    self.m_ctrlPts.append([_x, _y])
+                elif len(self.m_ctrlPts) == 2:
+                    self.m_ctrlPts.append([_x, _y])
+                elif len(self.m_ctrlPts) == 3:
+                    self.m_ctrlPts.append([_x, _y])
+                    isComplete = True
         return isComplete
 
     # Curve (temporary and finalized)
@@ -58,6 +76,27 @@ class AppCurveCollector:
         self.m_ctrlPts = []
         self.m_tempCurve = []
         return curve
+
+    # Create Mesh
+    def createMesh(self, _patch, _distance):
+        # Get the bounding box of the selected patch
+        xMin, xMax, yMin, yMax = _patch.getBoundBox()
+
+        # Print the bounding box
+        print("Bounding Box: ", xMin, xMax, yMin, yMax)
+        print("Distance Between Points: ", _distance)
+
+        # Get all points inside the bounding box
+        all_points = []
+        for x in range(int(xMin), int(xMax), int(_distance)):
+            for y in range(int(yMin), int(yMax), int(_distance)):
+                all_points.append([x, y])
+
+        # Get all points inside the patch
+        for point in all_points:
+            hetool_point = Point(point[0], point[1])
+            if _patch.isPointInside(hetool_point):
+                Hetool.insertPoint(hetool_point)
 
     # Update temporary curve (mouse tracking)
     def update(self, _x, _y):
@@ -87,3 +126,33 @@ class AppCurveCollector:
                         + t**2 * self.m_ctrlPts[1][1]
                     )
                     self.m_tempCurve.append([ptx, pty])
+        elif self.m_curveType == "Rectangular":
+            if len(self.m_ctrlPts) == 0:
+                pass
+            elif len(self.m_ctrlPts) == 1:
+                self.m_tempCurve = [
+                    self.m_ctrlPts[0],
+                    [self.m_ctrlPts[0][0], _y],
+                    [_x, _y],
+                    [_x, self.m_ctrlPts[0][1]],
+                    self.m_ctrlPts[0],
+                ]
+        elif self.m_curveType == "Quadrilateral":
+            # draw a rectangle one vertex at a time
+            if len(self.m_ctrlPts) == 0:
+                pass
+            elif len(self.m_ctrlPts) == 1:
+                # draw line
+                self.m_tempCurve = [self.m_ctrlPts[0], [_x, _y]]
+            elif len(self.m_ctrlPts) == 2:
+                # draw triangle
+                self.m_tempCurve = [self.m_ctrlPts[0], self.m_ctrlPts[1], [_x, _y]]
+            elif len(self.m_ctrlPts) == 3:
+                # draw rectangle
+                self.m_tempCurve = [
+                    self.m_ctrlPts[0],
+                    self.m_ctrlPts[1],
+                    self.m_ctrlPts[2],
+                    [_x, _y],
+                    self.m_ctrlPts[0],
+                ]
--- hetool_modeler/window.py
+++ hetool_modeler/window.py
@@ -3,6 +3,8 @@ from OpenGL.GL import *
 from PyQt5.QtGui import *
 from hetool.include.hetool import Hetool
 from canvas import AppCanvas
+from dialogs import *
+from constants import MIN_MESH_DISTANCE
 
 
 class AppWindow(QMainWindow):
@@ -22,6 +24,22 @@ class AppWindow(QMainWindow):
         tb.addAction(addLine)
         addBezier2 = QAction("Add Bezier", self)
         tb.addAction(addBezier2)
+        addRectangular = QAction("Add Rectangular", self)
+        tb.addAction(addRectangular)
+        addQuadrilateral = QAction("Add Quadrilateral", self)
+        tb.addAction(addQuadrilateral)
+        createMesh = QAction("Create Mesh", self)
+        tb.addAction(createMesh)
+        select = QAction("Select", self)
+        tb.addAction(select)
+        selectMultiple = QAction("Select Multiple", self)
+        tb.addAction(selectMultiple)
+        delete = QAction("Delete", self)
+        tb.addAction(delete)
+        undo = QAction("Undo", self)
+        tb.addAction(undo)
+        redo = QAction("Redo", self)
+        tb.addAction(redo)
         tb.actionTriggered[QAction].connect(self.tbpressed)
 
     # ToolBar Pressed Function
@@ -33,3 +51,30 @@ class AppWindow(QMainWindow):
             self.m_canvas.setState("Collect", "Line")
         elif _action.text() == "Add Bezier":
             self.m_canvas.setState("Collect", "Bezier2")
+        elif _action.text() == "Add Rectangular":
+            self.m_canvas.setState("Collect", "Rectangular")
+        elif _action.text() == "Add Quadrilateral":
+            self.m_canvas.setState("Collect", "Quadrilateral")
+        elif _action.text() == "Create Mesh":
+            if len(Hetool.getSelectedPatches()) == 0:
+                NoPatchSelectedDialog().exec_()
+            else:
+                dialog = CreateMeshDialog()
+                dialog.exec_()
+                distance = dialog.getDistance()
+
+                if distance >= MIN_MESH_DISTANCE:
+                    self.m_canvas.setState("CreateMesh", distance)
+        elif _action.text() == "Select":
+            self.m_canvas.setState("Select")
+        elif _action.text() == "Select Multiple":
+            self.m_canvas.setState("SelectMultiple")
+        elif _action.text() == "Delete":
+            Hetool.delSelectedEntities()
+            self.m_canvas.update()
+        elif _action.text() == "Undo":
+            Hetool.undo()
+            self.m_canvas.update()
+        elif _action.text() == "Redo":
+            Hetool.redo()
+            self.m_canvas.update()
